673efa20d39b150bdf888ae60889efac16c0cf3e
Rustam Nikonchuk <rustam.nikonchuk@yandex.ru>
2023-05-31 17:22:53 +0300

    chore: commit

--- src/Pages/Blogs.tsx
+++ src/Pages/Blogs.tsx
@@ -3,23 +3,23 @@ import axios from "axios";
 
 const Blogs = () => {
 
-    let XDomainRequest = 'https://draft-five.vercel.app/blogs';
-    let XHR = ("onload" in new XMLHttpRequest()) ? XMLHttpRequest : XDomainRequest;
-
-    // @ts-ignore
-    let xhr = new XHR();
-
-    xhr.open('GET', 'https://forms.yandex.ru/cloud/6476ff3cc417f301c195c8d1/', true);
-
-    xhr.onload = function() {
-        alert( this.responseText );
-    }
-
-    xhr.onerror = function() {
-        alert( 'Ошибка ' + this.status );
-    }
-
-    xhr.send()
+    // let XDomainRequest = 'https://draft-five.vercel.app/blogs';
+    // let XHR = ("onload" in new XMLHttpRequest()) ? XMLHttpRequest : XDomainRequest;
+    //
+    // // @ts-ignore
+    // let xhr = new XHR();
+    //
+    // xhr.open('GET', 'https://forms.yandex.ru/cloud/6476ff3cc417f301c195c8d1/', true);
+    //
+    // xhr.onload = function() {
+    //     alert( this.responseText );
+    // }
+    //
+    // xhr.onerror = function() {
+    //     alert( 'Ошибка ' + this.status );
+    // }
+    //
+    // xhr.send()
 
 
     async function fetchYa() {
@@ -40,7 +40,7 @@ const Blogs = () => {
     return (
         <div className={'wrapper'}>
             <h1>Blog Articles</h1>
-            <button onClick={fetchYa} style={{background: 'skyblue'}}>GET POSTS</button>
+            <button onClick={fetchYa} style={{background: 'pink'}}>GET POSTS</button>
         </div>
     );
 };
